ec7ebdb9dbfd76cd4976c92bf6065ce7bd465a47
Cadenviv07 <cva701281@gmail.com>
2025-11-23 00:11:39 -0500

    Changed ui for simplicity

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+import { Link } from "react-router-dom";
+
 
 export default function DashboardPage() {
   const [workouts, setWorkouts] = useState([]);
@@ -17,20 +18,22 @@ export default function DashboardPage() {
     };
     fetchWorkouts();
   }, [email]);
-
-  return (
-    <div className="p-8">
+Return(
+  <div className="p-8">
       <h1 className="text-2xl font-semibold mb-6">Workouts</h1>
 
       <ul className="space-y-4">
         {workouts.map((w) => (
-          <li key={w.id} className="p-4 bg-gray-100 rounded-lg">
-            <h2 className="text-xl font-bold">{w.name}</h2>
-
-            {/* Muscle groups */}
-            <p className="text-gray-700 mt-1">
-              {w.muscleTargets.join(", ")}
-            </p>
+          <li key={w.id}>
+            <Link
+              to={`/workout/${w.id}`}
+              className="block p-4 bg-gray-100 rounded-lg hover:bg-gray-200"
+            >
+              <h2 className="text-xl font-bold">{w.name}</h2>
+              <p className="text-gray-700 mt-1">
+                {w.muscleGroups.join(", ")}
+              </p>
+            </Link>
           </li>
         ))}
       </ul>
