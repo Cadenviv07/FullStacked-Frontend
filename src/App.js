b8e65a2a20758497f6f2349e9e1ce37c76704241
Cadenviv07 <cva701281@gmail.com>
2025-12-22 22:54:29 -0500

    further logout logic

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ import WorkoutPage from "./pages/WorkoutPage";
 import ExercisePage from "./pages/ExercisePage";
 
 function App() {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interceptor = axios.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          
+            if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            console.warn("Session expired or invalid token. Logging out...");
+            
+            localStorage.removeItem("token");
+            localStorage.removeItem("email");
+
+           
+            if (window.location.pathname !== "/login") {
+                window.location.href = "/login";
+            }
+            }
+            return Promise.reject(error);
+        }
+        );
+
+        return () => axios.interceptors.response.eject(interceptor);
+    }, [navigate]);
+
     return (
         <Router>
             <Routes>
